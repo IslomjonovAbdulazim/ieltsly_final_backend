e479e7e796418c31a4fba4b82a51486763df1635
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-09-27 00:57:12 +0500

    db are good

--- main.py
+++ main.py
@@ -59,4 +59,5 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.getenv("PORT", 8080))
+    print(f"🚀 Starting server on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
